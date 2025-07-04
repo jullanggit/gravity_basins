6fc7b97c23e4f90fd75fefe1fd04d949cdf508d7
Jul Lang <jullanggit@proton.me>
2025-07-04 19:22:39 +0200

    sdlfjsdlkfj

--- shader/src/lib.rs
+++ shader/src/lib.rs
@@ -9,12 +9,15 @@ use spirv_std::spirv;
 pub struct Data {
     pub gravitons: [Graviton; 32],
     pub num_gravitons: u32,
+    /// pad to 16 bytes, see `Graviton` for details
+    _pad: [u32; 3],
 }
 impl Data {
     pub fn new(gravitons: [Graviton; 32], num_gravitons: u32) -> Self {
         Self {
             gravitons,
             num_gravitons,
+            _pad: [0; 3],
         }
     }
 }
@@ -22,12 +25,20 @@ impl Data {
 #[derive(Pod, Zeroable, Clone, Copy)]
 #[repr(C)]
 pub struct Graviton {
-    pub position: [f32; 2],
+    // color is a vec3<f32> in wgpu, which has an alignment of 16 bytes, so we need to align the entire struct to that
     pub color: [f32; 3],
+    _pad0: u32,
+    pub position: [f32; 2],
+    _pad1: [u32; 2],
 }
 impl Graviton {
     pub fn new(position: [f32; 2], color: [f32; 3]) -> Self {
-        Self { position, color }
+        Self {
+            position,
+            color,
+            _pad0: 0,
+            _pad1: [0; 2],
+        }
     }
 }
 
